2c482de2f8080e9635ea45d5691bad2b3199474a
mchen1235 <michaelschen123@gmail.com>
2023-01-19 11:02:14 -0800

    Added blstm model

---
+++ arduino/smart_dog_collar/smart_dog_collar_model_data.h
@@ -0,0 +1,7 @@
+#ifndef SMART_DOG_COLLAR_MODEL_DATA_H_
+#define SMART_DOG_COLLAR_MODEL_DATA_H_
+
+extern const unsigned char sdc_blstm_model_data[];
+extern const int sdc_blstm_model_data_len;
+
+#endif
